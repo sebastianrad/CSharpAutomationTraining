a557cec0d37908d2d5301babc894166e7f42ffa7
Corina Cretu <corina.cretu@endava.com>
2020-07-22 00:21:40 +0300

    added browser class

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    public class Browser
+    {
+        //variable
+        public int WindowWidth = 1024;
+        public Browser()
+        {
+
+        }
+
+        public Browser (int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+        
+        public void ChangeWindowWidth(int NewWindowWidth)
+        {
+            WindowWidth = NewWindowWidth;
+        }
+    }
+}
