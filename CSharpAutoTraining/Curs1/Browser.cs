c9ba8086da2ea6241e26dd5994d419a8510342e2
iosiftamas <iosiftamas@yahoo.com>
2020-07-21 23:20:13 +0300

    added browser class

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    public class Browser
+    {
+        //variable
+        public int WindowWidth = 1024;
+
+        public void ChangeWindowWidth(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+
+    }
+}
