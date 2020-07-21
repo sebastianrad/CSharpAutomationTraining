29ccb34ee12106bd3ea51cb8f8553397668cd1ef
iosiftamas <iosiftamas@yahoo.com>
2020-07-22 01:27:34 +0300

    Deleted class Program, added Test class, modified class Browser in concordance with homework

--- CSharpAutoTraining/Curs1/Browser.cs
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -8,13 +8,34 @@ namespace CSharpAutoTraining.Curs1
 {
     public class Browser
     {
-        //variable
-        public int WindowWidth = 1024;
+        //variables
+        public int WindowHeight = 1024;
+        public int WindowWidth = 2000;
+
+
+        public Browser()
+        {
+           
+        }
+
+        public void ChangeWindowHeight(int newWindowHeight)
+        {
+            WindowHeight = newWindowHeight;
+        }
 
         public void ChangeWindowWidth(int newWindowWidth)
         {
             WindowWidth = newWindowWidth;
         }
 
+        public void DisplayWindowHeight()
+        {
+            Console.WriteLine("browser's newHeight: " + WindowHeight);
+        }
+
+        public void DisplayWindowWidth()
+        {
+            Console.WriteLine("browser's newWidth: " + WindowWidth);
+        }
     }
 }
