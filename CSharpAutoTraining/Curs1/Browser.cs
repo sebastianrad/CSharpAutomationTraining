c31046540bcb8184502f2839eccbb71cfa5a45de
sebastianrad <65756657+sebastianrad@users.noreply.github.com>
2020-07-23 00:30:50 +0300

    added browser class

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Browser
+    {
+        // variabila
+        public int WindowWith = 1024;
+
+        public void ChangeWindowWith(int newWindowWith)
+        {
+            WindowWith = newWindowWith;
+        }
+    }
+}
