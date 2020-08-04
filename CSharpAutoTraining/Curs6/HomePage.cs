90ff89a7ca1059218a2f2c022cdf1c3d43f5e89b
iosiftamas <iosiftamas@yahoo.com>
2020-08-04 03:14:26 +0300

    modified variable for Password label

--- CSharpAutoTraining/Curs6/HomePage.cs
+++ CSharpAutoTraining/Curs6/HomePage.cs
@@ -8,5 +8,14 @@ namespace CSharpAutoTraining.Curs6
 {
     class HomePage
     {
+        //variables
+        String Image = "//div[@id = 'header']/a/img"; // (header)
+        String HomeLink = "//header/ul/a[1]"; // (header)
+        String HTML = "//h1"; //(h1)
+        String Text = "//body/p"; // from page(body)
+        String Email = "//input[@id = 'email']"; // (input):
+        String Password = "//form/text()[2]"; // (text/label)
+        String Login = "//button[@id = 'Login']"; // button
+        String HomeFooter = "//footer/ul/li[1]"; // (footer)
     }
 }
